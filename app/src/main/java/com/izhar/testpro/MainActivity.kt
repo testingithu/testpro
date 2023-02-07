672708469f77b9f34314c90d9e035e6c378a32b5
Izhar <izfkjahdf>
2023-02-07 19:20:03 +0500

    first project

---
+++ app/src/main/java/com/izhar/testpro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.izhar.testpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
